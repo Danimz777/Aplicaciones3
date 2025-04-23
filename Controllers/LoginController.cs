74da001857f0cb214b6e7e559ba70f90fd7eb407
JAHENAO04 <andrhevil@gmail.com>
2025-04-22 19:28:47 -0500

    Update LoginController.cs

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,1 +1,23 @@
-﻿// commit
+using Servicios_6_8.Clases;
+using Examen_3.Models;  // Importa las clases necesarias del modelo de estudiantes
+using System.Linq;
+using System.Web.Http;
+using Servicios_6_8.Models;
+
+namespace Servicios_6_8.Controllers
+{
+    [RoutePrefix("api/Login")]
+    
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("IngresarEstudiante")]
+        public IQueryable<LoginRespuesta> IngresarEstudiante(Login login)
+        {
+            // Crear una instancia de clsEstudiante para manejar la autenticación del estudiante
+            clsEstudiante _Estudiante = new clsEstudiante();
+            _Estudiante.login = login;
+            return _Estudiante.Ingresar();
+        }
+    }
+}
